ee6f40329ac4b3f4ef796c2a265d8b43e5252256
Ayomide-22 <ayomideagboola777@gmail.com>
2022-11-04 11:55:53 +0100

    Code challenge

---
+++ 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter starting number");
+            double start =double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter end number");
+            double end =double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter gap to check");
+            double gap =double.Parse(Console.ReadLine());
+            int count=0;
+            for (var k = start; k <=end-gap; k++)
+            {
+                if(CheckPrime(k))
+                {
+                    if(CheckPrime(k+gap))
+                    {
+                        count++;
+                    }
+                }
+                if(count==1)
+                {
+                    Console.WriteLine($"{k} {k+gap}");
+                    break;
+                }
+            }
+            if(count==0)
+            {Console.WriteLine("There are are no pairs with this gap");}
+        }
+
+
+        static bool CheckPrime(double n)
+        {
+            double c=0;
+            if(n==0||n==1){return false;}
+            if(n%2==0){c=n/2;}
+            else{c=(n+1)/2;}
+            for(double j=2;j<=c;j++)
+            {
+                if(n%j==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter num 1");
+            int num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num 2");
+            int num2=int.Parse(Console.ReadLine());
+            if(num1<0 || num2<0)
+            {
+                Console.WriteLine("Both numbers bust be non-negative");
+                return;
+            }
+            int count=0;
+            while (num1!=0 && num2!=0)
+            {
+                if (num1>=num2)
+                {
+                    num1=num1-num2;
+                    count++;
+                }
+                else if(num2>num1)
+                {
+                    num2=num2-num1;
+                    count++;
+                }
+            }
+            Console.WriteLine($"It takes {count} operations");
+        }
+    }
+}
